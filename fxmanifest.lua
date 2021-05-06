3208d655c58cede03eb252f6e7b328619b4f35da
AgentofDeath <69638603+AgentofDeath@users.noreply.github.com>
2021-05-06 09:33:40 -0700

    upload
    will update and add sql later.

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+client_scripts {
+    'cl_hunting.lua'
+}
+
+
+server_scripts {
+	'sv_hunting.lua'
+ 
+}
---
+++ sv_hunting.lua
@@ -0,0 +1,47 @@
+
+ESX = nil
+
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+
+ESX.RegisterUsableItem('huntingknife', function(source)
+    TriggerClientEvent('AOD-huntingknife',source)
+end)
+
+ESX.RegisterUsableItem('huntingbait', function(source)
+    TriggerClientEvent('AOD-huntingbait', source)
+end)
+
+RegisterServerEvent('AOD-butcheranimal')
+AddEventHandler('AOD-butcheranimal', function(animal)
+    local source = source
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local boar = -832573324
+    local deer = -664053099
+    local cayote = 1682622302
+
+    if animal == boar then
+        local amount = math.random(5)
+        xPlayer.addInventoryItem('boarmeat', amount)
+        xPlayer.addInventoryItem('boartusk', 2)
+    elseif animal == deer then
+        local dmeat = math.random(5)
+        xPlayer.addInventoryItem('deerskin', 1)
+        xPlayer.addInventoryItem('deermeat', dmeat)
+    elseif animal == cayote then
+        local cmeat = math.random(5)
+        xPlayer.addInventoryItem('coyotefur', 1)
+        xPlayer.addInventoryItem('coyotemeat', cmeat)
+    else
+        print('exploit detected')
+    end
+end)
+
+
+RegisterServerEvent('AOD-hunt:TakeItem')
+AddEventHandler('AOD-hunt:TakeItem', function(item)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    xPlayer.removeInventoryItem(item, 1)
+
+end)
