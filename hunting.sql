63a302b162029333683df48d6b39eabfd7c3229f
AgentofDeath <69638603+AgentofDeath@users.noreply.github.com>
2021-05-07 08:05:16 -0500

    Create hunting.sql

---
+++ hunting.sql
@@ -0,0 +1,10 @@
+INSERT INTO `items` (name, label, weight) VALUES
+	('huntingknife','Hunting Knife', 2),
+	('huntingbait','Hunting Bait', 2),
+  ('boarmeat','Boar Meat', 2),
+	('boartusk','Boar Tusk', 2),
+  ('deerskin','Deer Skin', 2),
+	('deermeat','Deer Meat', 2),
+  ('coyotefur','Coyote Fur', 2),
+	('coyotemeat','Coyote Meat', 2)
+;
