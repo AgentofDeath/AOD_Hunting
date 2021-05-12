8bbb9739cfd927e7439ede3bc70054cbfd1d9f59
AgentofDeath <69638603+AgentofDeath@users.noreply.github.com>
2021-05-12 11:18:02 -0500

    add a limit sql for users not using weight

---
+++ limit.sql
@@ -0,0 +1,10 @@
+INSERT INTO `items` (name, label, limit) VALUES
+	('huntingknife','Hunting Knife', 1),
+	('huntingbait','Hunting Bait', 1),
+  	('boarmeat','Boar Meat', 20),
+	('boartusk','Boar Tusk', 20),
+  	('deerskin','Deer Skin', 20),
+	('deermeat','Deer Meat', 20),
+  	('coyotefur','Coyote Fur', 20),
+	('coyotemeat','Coyote Meat', 20)
+;
