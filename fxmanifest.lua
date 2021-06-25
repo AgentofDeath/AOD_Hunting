f363b20baaf7b661455e6b1e706c4ca54511c7c4
AgentofDeath <69638603+AgentofDeath@users.noreply.github.com>
2021-06-25 17:56:02 -0500

    fixed fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,8 +2,8 @@ fx_version 'cerulean'
 game 'gta5'
 description 'hunting activity for the ESX framework https://forum.cfx.re/t/aod-hunting-esx/3492981'
 author 'AgentofDeath'
-version '1.1.0'
+version '1.2.0'
 
-client_scripts {'cfg_hunting', 'cl_hunting.lua'}
+client_scripts {'cfg_hunting.lua', 'cl_hunting.lua'}
 
-server_scripts {'cfg_hunting','sv_hunting.lua'}
+server_scripts {'cfg_hunting.lua','sv_hunting.lua'}
