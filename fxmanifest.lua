143d58361443936df027250b049c64196967b586
AgentofDeath <69638603+AgentofDeath@users.noreply.github.com>
2021-06-25 17:55:11 -0500

    new manifest for update

--- fxmanifest.lua
+++
@@ -1,9 +0,0 @@
-fx_version 'cerulean'
-game 'gta5'
-description 'hunting activity for the ESX framework https://forum.cfx.re/t/aod-hunting-esx/3492981'
-author 'AgentofDeath'
-version '1.1.0'
-
-client_script 'cl_hunting.lua'
-
-server_script 'sv_hunting.lua'
